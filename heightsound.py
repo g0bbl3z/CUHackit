a37c2f7ce27b5076e7e8c4553a345e219d2a7c31
Cade <mack.cade1@gmail.com>
2019-01-26 17:51:32 -0500

    YES

--- heightsound.py
+++ heightsound.py
@@ -1,27 +1,31 @@
+import pygame, sys
+
 import sys, pygame, winsound
 
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
-handheight = 300
+size = width, height = 800, 500
+handheight = 50
 black = [0, 0 ,0]
 
+handSprite = pygame.image.load("resources/hand_sprite.png")
+spriteRect = handSprite.get_rect()
 screen = pygame.display.set_mode(size)
 
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                handheight += 50
-            if event.key == pygame.K_DOWN:
-                handheight += -50
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP < 1000:
+                    handheight += 5
+                if event.key == pygame.K_DOWN and handheight > 5:
+                    handheight += -5
+                if event.key == pygame.K_ESCAPE:
+                        sys.exit()
 
-
-    winsound.Beep(handheight, 100)
-
-    screen.fill(black)
-
-    pygame.display.flip()
+        winsound.Beep(300 + handheight, 100)
+        screen.fill(black)
+        screen.blit(handSprite, (200, 400 - handheight))
+        pygame.display.flip()
