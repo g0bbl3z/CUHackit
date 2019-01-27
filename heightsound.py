3acfc9e3daa4e36888edb0539d2191af9a4d4a87
Cade <mack.cade1@gmail.com>
2019-01-26 19:04:42 -0500

    updated

--- heightsound.py
+++ heightsound.py
@@ -1,6 +1,6 @@
 
 
-import sys, pygame, winsound
+import sys, pygame, winsound, time
 pygame.init()
 
 size = width, height = 800, 500
@@ -11,18 +11,34 @@ handSprite = pygame.image.load("resources/hand_sprite.png")
 spriteRect = handSprite.get_rect()
 screen = pygame.display.set_mode(size)
 
+cMajorFile = pygame.mixer.Sound("resources/cMajor.mp3")
+aMajorFile = pygame.mixer.Sound("resources/aMajor.mp3")
+gMajorFile = pygame.mixer.Sound("resources/gMajor.mp3")
+
+
 while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP < 1000:
-                    handheight += 5
-                if event.key == pygame.K_DOWN and handheight > 5:
-                    handheight += -5
-                if event.key == pygame.K_ESCAPE:
-                        sys.exit()
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_UP < 500:
+            #         handheight += 5
+            #     if event.key == pygame.K_DOWN and handheight > 5:
+            #         handheight += -5
+            #     if event.key == pygame.K_ESCAPE:
+            #             sys.exit()
 
+        # if handheight < 170:
+        #     # cMajorFile.stop()
+        #     cMajorFile.play()
+        # elif handheight < 370:
+        #     # aMajorFile.stop();
+        #     aMajorFile.play();
+        # else:
+        #     # gMajorFile.stop();
+        #     gMajorFile.play();
+
+        # handheight =
         winsound.Beep(300 + handheight, 100)
         screen.fill(black)
         screen.blit(handSprite, (200, 400 - handheight))
